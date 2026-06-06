524a5d1b3df34db8bf8e7b8cd7cbf52480ebd763
Samuel Olagunju <git@samola.net>
2026-06-06 17:27:41 -0400

    added md-pdf.nvim plugin for latex preview

---
+++ .config/nvim/lua/custom/plugins/md-pdf.lua
@@ -0,0 +1,19 @@
+return {
+  "arminveres/md-pdf.nvim",
+  branch = "main", -- you can assume that main is somewhat stable until releases will be made
+  lazy = true,
+  keys = {
+    {
+      "<leader>,",
+      function()
+        require("md-pdf").convert_md_to_pdf()
+      end,
+      desc = "Markdown preview",
+    },
+  },
+  ---@type md-pdf.config
+  opts = {
+    toc = false,
+    pdf_engine = "xelatex",
+  },
+}
