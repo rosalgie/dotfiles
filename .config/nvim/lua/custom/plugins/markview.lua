ed15e686f7ff9e47aafc6c52e20f4f8e14e3608d
Samuel Olagunju <git@samola.net>
2026-06-06 15:00:15 -0400

    Enable latex and html support in markview

--- .config/nvim/lua/custom/plugins/markview.lua
+++ .config/nvim/lua/custom/plugins/markview.lua
@@ -1,4 +1,68 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
+
+  config = function()
+    require("markview").setup({
+      html = {
+        enable = true,
+
+        container_elements = {
+          enable = true,
+        },
+
+        headings = {
+          enable = true,
+        },
+
+        void_elements = {
+          enable = true,
+        },
+      },
+
+      latex = {
+        enable = true,
+
+        blocks = {
+          enable = true,
+        },
+
+        inlines = {
+          enable = true,
+        },
+
+        commands = {
+          enable = true,
+        },
+
+        escapes = {
+          enable = true,
+        },
+
+        fonts = {
+          enable = true,
+        },
+
+        parenthesis = {
+          enable = true,
+        },
+
+        subscripts = {
+          enable = true,
+        },
+
+        superscripts = {
+          enable = true,
+        },
+
+        symbols = {
+          enable = true,
+        },
+
+        texts = {
+          enable = true,
+        },
+      },
+    })
+  end,
 }
