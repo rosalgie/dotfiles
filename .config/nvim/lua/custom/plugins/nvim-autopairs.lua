a77cb90b93c3fd38c5befa5fc99d0eefe31a15c0
Samuel Olagunju <git@samola.net>
2026-06-07 14:22:56 -0400

    add latex markdown autopair

--- .config/nvim/lua/custom/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/custom/plugins/nvim-autopairs.lua
@@ -2,8 +2,13 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
+    config = function()
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+
+      npairs.setup({})
+
+      npairs.add_rule(Rule("$", "$", { "markdown" }))
+    end,
   },
 }
